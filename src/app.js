cfdcd32dfcf3a67b7d5493ddff0648c364500b6d
carlospmendiola <carlos@carlospmendiola.com>
2026-05-17 14:41:48 +0200

    Primner commit

--- src/app.js
+++ src/app.js
@@ -3,8 +3,19 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+//configuramos la carpeta estatica (funcion middleware)
+app.use(express.static(__dirname + "/public"))
+
+//Establecer ejs como template engine
+app.set('view engine' , 'ejs')
+
+//Estableder cual va a ser la carpeta de vistas
+app.set("views",__dirname + "/views");
+
+
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send("<h1>Práctica JWT</h1>");
@@ -60,7 +71,11 @@ app.post('/auth', (req, res) => {
 })
 
 
-
+app.use((req,res,next)=>{
+  res.status(404).render("404",{
+            titulo:'error 404'
+        }) 
+});
 
 
 
